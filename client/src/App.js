9073c2bb0bf98a9c31b86f3eb326978d70fc75ef
nidhal <nidhaldjerbi@gmail.com>
2018-12-04 00:28:18 +0100

    interact front with back

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,72 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ response: res.express }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch('/api/contact');
+    const body = await response.json();
+    if (response.status !== 200) throw Error(body.message);
+    return body;
+  };
+
+
+  handleSubmit = async e => {
+    e.preventDefault();
+    let nom = ReactDOM.findDOMNode(this.refs.nom).value;
+    let email = ReactDOM.findDOMNode(this.refs.email).value;
+    let subject = ReactDOM.findDOMNode(this.refs.subject).value;
+    let message = ReactDOM.findDOMNode(this.refs.message).value;
+    
+    const response = await fetch('/api/contact', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name: nom,
+                            email: email,
+                            subject: subject,
+                            message: message
+                          }),
+    });
+    const body = await response.text();
+    this.setState({ responseToPost: body });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Nom:
+          <input type="text" ref="nom"  />
+        </label>
+        <label>
+          Email:
+          <input type="email" ref="email"  />
+        </label>
+        <label>
+          Sujet:
+          <input type="text" ref="subject"  />
+        </label>
+        <label>
+          Message:
+          <textarea  ref="message" />  
+        </label>
+        <input type="submit" value="Contact" />
+      </form>
     );
   }
 }
-
 export default App;
--- models/contact.js
+++ models/contact.js
@@ -13,6 +13,10 @@ const contactSchema = Schema({
         match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Email not valid'],
         required: 'Email required'
     },
+    subject: {
+        type: String,
+        required: 'Subject required'
+    },
     message: {
         type: String,
         required: 'Message required'
